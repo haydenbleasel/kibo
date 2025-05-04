219415a9cb00cfde5e4b53f04d24bd6579df34aa
Hayden Bleasel <hello@haydenbleasel.com>
2025-05-03 18:27:44 -0700

    Add marquee to hero

--- apps/docs/examples/hero.tsx
+++ apps/docs/examples/hero.tsx
@@ -1,10 +1,25 @@
 'use client';
 
 import {
+  SiAmazon,
+  SiApple,
+  SiFacebook,
+  SiGithub,
+  SiGoogle,
+  SiInstagram,
+  SiYoutube,
+} from '@icons-pack/react-simple-icons';
+import {
   Announcement,
   AnnouncementTag,
   AnnouncementTitle,
 } from '@repo/announcement';
+import {
+  Marquee,
+  MarqueeContent,
+  MarqueeFade,
+  MarqueeItem,
+} from '@repo/marquee';
 import { Button } from '@repo/shadcn-ui/components/ui/button';
 import {
   VideoPlayer,
@@ -20,6 +35,43 @@ import {
 } from '@repo/video-player';
 import Link from 'next/link';
 
+const logos = [
+  {
+    name: 'GitHub',
+    icon: SiGithub,
+    url: 'https://github.com',
+  },
+  {
+    name: 'Facebook',
+    icon: SiFacebook,
+    url: 'https://facebook.com',
+  },
+  {
+    name: 'Google',
+    icon: SiGoogle,
+    url: 'https://google.com',
+  },
+  {
+    name: 'Amazon',
+    icon: SiAmazon,
+    url: 'https://amazon.com',
+  },
+  {
+    name: 'Apple',
+    icon: SiApple,
+    url: 'https://apple.com',
+  },
+  {
+    name: 'Instagram',
+    icon: SiInstagram,
+    url: 'https://instagram.com',
+  },
+  {
+    name: 'YouTube',
+    icon: SiYoutube,
+    url: 'https://youtube.com',
+  },
+];
 const Example = () => (
   <div className="flex flex-col gap-16 px-8 py-24 text-center">
     <div className="flex flex-col items-center justify-center gap-8">
@@ -48,6 +100,26 @@ const Example = () => (
         </Button>
       </div>
     </div>
+    <section className="flex flex-col items-center justify-center gap-8 rounded-xl bg-secondary py-8 pb-18">
+      <p className="mb-0 text-balance font-medium text-muted-foreground">
+        Trusted by developers from leading companies
+      </p>
+      <div className="flex size-full items-center justify-center">
+        <Marquee>
+          <MarqueeFade side="left" className="from-secondary" />
+          <MarqueeFade side="right" className="from-secondary" />
+          <MarqueeContent pauseOnHover={false}>
+            {logos.map((logo) => (
+              <MarqueeItem key={logo.name} className="mx-16 size-12">
+                <Link href={logo.url}>
+                  <logo.icon className="size-full" />
+                </Link>
+              </MarqueeItem>
+            ))}
+          </MarqueeContent>
+        </Marquee>
+      </div>
+    </section>
     <VideoPlayer className="overflow-hidden rounded-lg border">
       <VideoPlayerContent
         slot="media"
