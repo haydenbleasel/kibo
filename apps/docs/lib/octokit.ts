088d906d581a32b39018bf87d3c0dfe23da70021
Hayden Bleasel <hello@haydenbleasel.com>
2025-05-15 15:56:12 -0700

    Misc fixes

--- apps/docs/lib/octokit.ts
+++ apps/docs/lib/octokit.ts
@@ -1,5 +1,5 @@
-import { Octokit } from 'octokit';
+import { Octokit } from '@octokit/rest';
 
-export const octokit = new Octokit({
+export const octokit: Octokit = new Octokit({
   auth: process.env.GITHUB_TOKEN,
 });
