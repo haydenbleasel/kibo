25a63a195d7bdfda82a9a8e7e0ef3e67ae085747
Hayden Bleasel <hello@haydenbleasel.com>
2025-10-08 20:54:49 -0700

    Initial Input OTP draft

---
+++ packages/comps/input-otp/standard/input-otp-standard-4.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { useState } from "react";
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSeparator,
+  InputOTPSlot,
+} from "@/components/ui/input-otp";
+
+export const title = "Controlled OTP Input";
+
+const Example = () => {
+  const [value, setValue] = useState("");
+
+  return (
+    <div className="space-y-2">
+      <InputOTP maxLength={6} value={value} onChange={setValue}>
+        <InputOTPGroup>
+          <InputOTPSlot index={0} />
+          <InputOTPSlot index={1} />
+          <InputOTPSlot index={2} />
+        </InputOTPGroup>
+        <InputOTPSeparator />
+        <InputOTPGroup>
+          <InputOTPSlot index={3} />
+          <InputOTPSlot index={4} />
+          <InputOTPSlot index={5} />
+        </InputOTPGroup>
+      </InputOTP>
+      <div className="text-center text-sm">
+        {value === "" ? (
+          <>Enter your one-time password.</>
+        ) : (
+          <>You entered: {value}</>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Example;
