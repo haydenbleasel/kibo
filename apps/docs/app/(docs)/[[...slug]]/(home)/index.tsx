768d6ddecee92a8bfc3a80082ac11b85feae11be
Hayden Bleasel <hello@haydenbleasel.com>
2025-10-12 20:59:52 -0700

    Update homepage

--- apps/docs/app/(docs)/[[...slug]]/(home)/index.tsx
+++ apps/docs/app/(docs)/[[...slug]]/(home)/index.tsx
@@ -2,13 +2,15 @@ import { Blocks } from "./components/blocks";
 import { CallToAction } from "./components/call-to-action";
 import { Components } from "./components/components";
 import { Hero } from "./components/hero";
+import { Patterns } from "./components/patterns";
 import { Tweets } from "./components/tweets";
 
 const Home = () => (
-  <div className="mt-[var(--fd-nav-height)] w-full p-4">
+  <div className="mt-[var(--fd-nav-height)] grid w-full gap-16 p-4 sm:gap-24">
     <Hero />
     <Components />
     <Blocks />
+    <Patterns />
     <Tweets />
     <CallToAction />
   </div>
