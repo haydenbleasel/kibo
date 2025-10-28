a9fd0a62264c380d7e52f7f8a7d5776d4103c969
Hayden Bleasel <hello@haydenbleasel.com>
2025-10-28 12:12:21 -0700

    Remove unused NEXT_RUNTIME

--- apps/docs/env.ts
+++ apps/docs/env.ts
@@ -6,9 +6,6 @@ export const env = createEnv({
   extends: [vercel()],
   server: {
     ANALYZE: z.string().optional(),
-
-    // Added by Vercel
-    NEXT_RUNTIME: z.enum(["nodejs", "edge"]).optional(),
   },
   client: {
     NEXT_PUBLIC_GA_MEASUREMENT_ID: z
@@ -20,7 +17,6 @@ export const env = createEnv({
   },
   runtimeEnv: {
     ANALYZE: process.env.ANALYZE,
-    NEXT_RUNTIME: process.env.NEXT_RUNTIME,
     NEXT_PUBLIC_GA_MEASUREMENT_ID: process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID,
     NEXT_PUBLIC_LOGO_DEV_TOKEN: process.env.NEXT_PUBLIC_LOGO_DEV_TOKEN,
   },
