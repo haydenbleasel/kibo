553e6b02b4e9d7daa33f2d1772051449e22bbfd3
Hayden Bleasel <hello@haydenbleasel.com>
2025-10-28 12:10:21 -0700

    Remove Sentry

--- apps/docs/env.ts
+++ apps/docs/env.ts
@@ -11,10 +11,6 @@ export const env = createEnv({
 
     // Added by Vercel
     NEXT_RUNTIME: z.enum(["nodejs", "edge"]).optional(),
-
-    // Added by Sentry Integration, Vercel Marketplace
-    SENTRY_ORG: z.string().min(1).optional(),
-    SENTRY_PROJECT: z.string().min(1).optional(),
   },
   client: {
     NEXT_PUBLIC_GA_MEASUREMENT_ID: z
@@ -23,9 +19,6 @@ export const env = createEnv({
       .startsWith("G-")
       .optional(),
     NEXT_PUBLIC_LOGO_DEV_TOKEN: z.string().min(1).optional(),
-
-    // Added by Sentry Integration, Vercel Marketplace
-    NEXT_PUBLIC_SENTRY_DSN: z.string().min(1).url().optional(),
   },
   runtimeEnv: {
     ANALYZE: process.env.ANALYZE,
@@ -33,9 +26,6 @@ export const env = createEnv({
     NEXT_PUBLIC_GA_MEASUREMENT_ID: process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID,
     BETTERSTACK_API_KEY: process.env.BETTERSTACK_API_KEY,
     BETTERSTACK_URL: process.env.BETTERSTACK_URL,
-    SENTRY_ORG: process.env.SENTRY_ORG,
-    SENTRY_PROJECT: process.env.SENTRY_PROJECT,
-    NEXT_PUBLIC_SENTRY_DSN: process.env.NEXT_PUBLIC_SENTRY_DSN,
     NEXT_PUBLIC_LOGO_DEV_TOKEN: process.env.NEXT_PUBLIC_LOGO_DEV_TOKEN,
   },
 });
