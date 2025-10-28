81e44f9eb52f8c2a20f753a89df3ccb495b54805
Hayden Bleasel <hello@haydenbleasel.com>
2025-10-28 12:10:38 -0700

    Remove unused env vars

--- apps/docs/env.ts
+++ apps/docs/env.ts
@@ -6,8 +6,6 @@ export const env = createEnv({
   extends: [vercel()],
   server: {
     ANALYZE: z.string().optional(),
-    BETTERSTACK_API_KEY: z.string().min(1).optional(),
-    BETTERSTACK_URL: z.string().min(1).url().optional(),
 
     // Added by Vercel
     NEXT_RUNTIME: z.enum(["nodejs", "edge"]).optional(),
@@ -24,8 +22,6 @@ export const env = createEnv({
     ANALYZE: process.env.ANALYZE,
     NEXT_RUNTIME: process.env.NEXT_RUNTIME,
     NEXT_PUBLIC_GA_MEASUREMENT_ID: process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID,
-    BETTERSTACK_API_KEY: process.env.BETTERSTACK_API_KEY,
-    BETTERSTACK_URL: process.env.BETTERSTACK_URL,
     NEXT_PUBLIC_LOGO_DEV_TOKEN: process.env.NEXT_PUBLIC_LOGO_DEV_TOKEN,
   },
 });
